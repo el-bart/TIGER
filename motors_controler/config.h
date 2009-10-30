ecbac96b982b26ab8166e0f88d0241628f17672d
bartek szurgot <el-bart@tweety>
2009-10-30 13:26:51 +0100

    files from old repos.
    added:
      motors_controler/
      motors_controler/Makefile
      motors_controler/config.h
      motors_controler/motors_control.c
      motors_controler/uC_used.txt
      rt_ctl/
      rt_ctl/rt_ctl.brd
      rt_ctl/rt_ctl.sch

---
+++ motors_controler/config.h
@@ -0,0 +1,18 @@
+/*
+ * config.h
+ *
+ */
+#ifndef INCLUDE_CONFIG_H_FILE
+#define INCLUDE_CONFIG_H_FILE
+
+// units commonly used
+#define KHz   1000L
+#define MHz   (1000L*KHZ)
+
+// quarc frequency in [Hz]
+#define F_CPU (1L*MHz)
+
+
+
+#endif
+
---
+++ motors_controler/motors_control.c
@@ -0,0 +1,21 @@
+/*
+ * motors_control.c
+ *
+ */
+#include "config.h"         // this file must be included as first one!
+
+#include <avr/io.h>
+//#include <util/delay.h>     // <avr/delay.h> once
+
+
+
+//
+// MAIN
+//
+int main(void)
+{
+  // one big TODO !
+
+  return 0;
+} // main()
+
