c24de7a0c5c88bad6c9803251ed8c763ee57ed75
root <root@tweety>
2010-11-18 13:37:14 +0100

    fixed typos in code. implemented led-klinking.
    modified:
      src/uC/rt_board/config.h
      src/uC/rt_board/main.c

--- src/uC/rt_board/config.h
+++ src/uC/rt_board/config.h
@@ -7,12 +7,9 @@
 
 // units commonly used
 #define KHz   1000L
-#define MHz   (1000L*KHZ)
+#define MHz   (1000L*KHz)
 
 // quarc frequency in [Hz]
 #define F_CPU (1L*MHz)
 
-
-
 #endif
-
--- src/uC/rt_board/main.c
+++ src/uC/rt_board/main.c
@@ -5,7 +5,7 @@
 #include "config.h"         // this file must be included as first one!
 
 #include <avr/io.h>
-//#include <util/delay.h>     // <avr/delay.h> once
+#include <util/delay.h>     // <avr/delay.h> once
 
 
 
@@ -15,14 +15,19 @@
 int main(void)
 {
   // one big TODO !
-
   DDRD=(1 << PB5);
-  PORTD=_BV(5);
-
 
   for(;;)
   {
+    PORTD|=_BV(5);
+    for(int i=0; i<2; ++i)
+      _delay_ms(250);
+    PORTD&=~_BV(5);
+    for(int i=0; i<2; ++i)
+      _delay_ms(250);
   }
+
+  for(;;) { }
 
   return 0;
 } // main()
