e87f5b1ba12e72b04b87d1dc4cd9b07afa9da772
bartek szurgot <el-bart@tweety>
2010-12-04 20:31:54 +0100

    USART is now working.
    modified:
      src/uC/rt_board/usart.c

--- src/uC/rt_board/usart.c
+++ src/uC/rt_board/usart.c
@@ -12,35 +12,50 @@
 
 #define USART_UBRR(baud,f) ( ((f)/((baud)*16L)) -1 )
 
-
 // TODO: add test for baud-rate error and show error if value is over 1%
 
+//
 // I/O queues
-static struct queue_data in_q;
-static struct queue_data out_q;
+//
+static struct queue_data g_in_q;
+static struct queue_data g_out_q;
 
-SIGNAL(SIG_UART_RECV)   // USART RX interrupt
+
+static inline void send_data_impl(void)
 {
-  unsigned char c;
-  c = UDR;
-  // TODO
+  uassert( UCSRA & _BV(UDRE) );
+  UDR=queue_pop(&g_out_q);
 }
 
-SIGNAL(SIG_UART_TRANS)  // USART TX interrupt
+// USART RX completed interrupt
+ISR(USART_RXC_vect)
 {
-  // TODO
+  const uint8_t c=UDR;      // read form hardware ASAP
+  if( queue_full(&g_in_q) ) // if queue is full, drop last element
+    queue_pop(&g_in_q);
+  queue_push(&g_in_q, c);   // enqueue new byte
 }
 
-SIGNAL(SIG_UART_DATA)   // USART data register is empty interrupt
+// USART TX completed interrupt
+ISR(USART_TXC_vect)
 {
-  // TODO
+  if( !queue_empty(&g_out_q) )  // if have something to send
+    send_data_impl();
+}
+
+// USART data register is empty interrupt
+ISR(USART_UDRE_vect)
+{
+  UCSRB&=~_BV(UDRIE);           // data registry empty - disable interrupt
+  if( queue_size(&g_out_q)>0 )  // if data register is empty and we have data to send
+    send_data_impl();           // we can send it now
 }
 
 
 void usart_init(void)
 {
-  queue_init(&in_q);
-  queue_init(&out_q);
+  queue_init(&g_in_q);
+  queue_init(&g_out_q);
 
   // clock devider register (computed from baud rate and oscilator frequency)
   UBRRH=(uint8_t)( (USART_UBRR(USART_BAUD, F_CPU)>>8) & 0x00FF );
@@ -48,22 +63,19 @@ void usart_init(void)
   // set single speed
   UCSRA =0x00;
   UCSRA&=~_BV(U2X);
-  // TODO: this code is still broken.
-goto test_this;
 
   // enable interrupts
   UCSRB|= _BV(RXCIE);   // RX complete
   UCSRB|= _BV(TXCIE);   // TX complete
-  // TODO: this is invalid, since this interrupt loops forever
-  //UCSRB|= _BV(UDRIE);   // data registry empty
   // enable transciever
   UCSRB|= _BV(RXEN);    // RX enable
   UCSRB|= _BV(TXEN);    // TX enable
 
   // configure 8-bit transmition mode
+  // NOTE: there is a bug in spec at this point - UCSZ[01] must be 0 not 1 here!
   UCSRB&=~_BV(UCSZ2);   // 0
-  UCSRC|= _BV(UCSZ1);   // 1
-  UCSRC|= _BV(UCSZ0);   // 1
+  UCSRC&=~_BV(UCSZ1);   // 0 (!)
+  UCSRC&=~_BV(UCSZ0);   // 0 (!)
   // configure no parity mode
   UCSRC&=~_BV(UPM1);
   UCSRC&=~_BV(UPM0);
@@ -74,14 +86,20 @@ goto test_this;
   UCSRC&=~_BV(URSEL);   // synchronous mode
   UCSRC&=~_BV(UCPOL);   // clock polarity mode
 
-test_this:
-  // Enable receiver and transmitter; enable RX interrupt
-  UCSRB = (1 << RXEN) | (1 << TXEN) | (1 << RXCIE);
+  // configure proper pins as in (RX) and out (TX)
+  DDRD &=~_BV(PD0);     // RX as in
+  PORTD|= _BV(PD0);     // RX high with pull-up
+  DDRD |= _BV(PD1);     // TX as out
 }
 
 void usart_send(uint8_t b)
 {
-  // TODO
+  while( queue_full(&g_out_q) );    // wait for space in queue
+  queue_push(&g_out_q, b);          // enqueue next char to send
+  if( queue_size(&g_out_q)==1 )     // this might be first char to send
+    UCSRB|=_BV(UDRIE);              // signal on data registry empty.
+                                    // if transmition has not yet started this will
+                                    // send initial (first) byte as soon as USART is ready
 }
 
 void usart_send_array(uint8_t *b, size_t size)
@@ -93,13 +111,11 @@ void usart_send_array(uint8_t *b, size_t size)
 
 size_t usart_inqueue_size(void)
 {
-  // TODO
-  return 0;
+  return queue_size(&g_in_q);       // return input queue size
 }
 
 uint8_t usart_receive(void)
 {
-  while( usart_inqueue_size()==0 ); // wait for the data
-  // TODO
-  return 0;
+  while( queue_empty(&g_in_q) );    // wait for the data
+  return queue_pop(&g_in_q);        // return read data
 }
